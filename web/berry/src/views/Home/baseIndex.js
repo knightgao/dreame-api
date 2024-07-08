d2df725e4e8520f8c843fec417acbcaea802fb6f
knight.gao <gaoye@dreame.tech>
2024-07-08 14:56:16 +0800

    feat: 修改主题

--- web/berry/src/views/Home/baseIndex.js
+++ web/berry/src/views/Home/baseIndex.js
@@ -1,6 +1,5 @@
 import { Box, Typography, Button, Container, Stack } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
-import { GitHub } from '@mui/icons-material';
 
 const BaseIndex = () => (
   <Box
@@ -20,18 +19,8 @@ const BaseIndex = () => (
             </Typography>
             <Typography variant="h4" sx={{ fontSize: '1.5rem', color: '#fff', lineHeight: 1.5 }}>
               All in one 的 OpenAI 接口 <br />
-              整合各种 API 访问方式 <br />
-              一键部署，开箱即用
+              整合各种 API 访问方式
             </Typography>
-            <Button
-              variant="contained"
-              startIcon={<GitHub />}
-              href="https://github.com/knightgao/dreame-api"
-              target="_blank"
-              sx={{ backgroundColor: '#24292e', color: '#fff', width: 'fit-content', boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)' }}
-            >
-              GitHub
-            </Button>
           </Stack>
         </Grid>
       </Grid>
